5cbc77a9e38694b2f65216500cacb8b0b9b341e7
Negue <eugen.bolz@gmail.com>
2015-06-20 20:40:53 +0200

    Update Gradle Version / Add Databinding & other libraries

---
+++ Habitica/src/com/habitrpg/android/habitica/HabiticaApplication.java
@@ -0,0 +1,18 @@
+package com.habitrpg.android.habitica;
+
+import android.app.Application;
+
+import com.instabug.library.Instabug;
+
+/**
+ * Created by Negue on 14.06.2015.
+ */
+public class HabiticaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Instabug.initialize(this, "a5aa5f471a9cd8a958c0c55181172655");
+    }
+}
