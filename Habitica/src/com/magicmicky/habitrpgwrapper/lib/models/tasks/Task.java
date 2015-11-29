fc779800b5bdf0be6ea72f60dd32ed6fb61233b3
Phillip Thelen <viirus@pherth.net>
2015-11-29 17:34:04 +0100

    correct daily checklist rendering

--- Habitica/src/com/magicmicky/habitrpgwrapper/lib/models/tasks/Task.java
+++ Habitica/src/com/magicmicky/habitrpgwrapper/lib/models/tasks/Task.java
@@ -485,6 +485,6 @@ public class Task extends BaseModel {
     }
 
     public Boolean isChecklistDisplayActive(int offset) {
-        return this.isDisplayedActive(offset) || (this.checklist.size() != this.getCompletedChecklistCount());
+        return this.isDisplayedActive(offset) && (this.checklist.size() != this.getCompletedChecklistCount());
     }
 }
