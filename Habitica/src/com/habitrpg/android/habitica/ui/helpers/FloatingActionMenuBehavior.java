2bc41dc5208ea4f92649dcfdb092a6fbc4e74ab2
Darron Lumley <dlumley@iinet.net.au>
2015-12-17 20:06:06 +1100

    Removed unnecessary import

--- Habitica/src/com/habitrpg/android/habitica/ui/helpers/FloatingActionMenuBehavior.java
+++ Habitica/src/com/habitrpg/android/habitica/ui/helpers/FloatingActionMenuBehavior.java
@@ -8,7 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.github.clans.fab.FloatingActionMenu;
