007be89c5e7d2d7aa17e0bcad268f1ed2878ae66
Michael Kaiser <patheticpat@googlemail.com>
2016-01-25 18:09:02 +0100

    Don't animate health bar in party member list

--- Habitica/src/com/habitrpg/android/habitica/ui/AvatarWithBarsViewModel.java
+++ Habitica/src/com/habitrpg/android/habitica/ui/AvatarWithBarsViewModel.java
@@ -174,14 +174,7 @@ public class AvatarWithBarsViewModel implements View.OnClickListener {
         value = (float) Math.ceil(value);
         double percent = Math.min(1, value / valueMax);
 
-        if (percent == 1) {
-            valueBar.setWeightToShow(1);
-            valueBar.setWeightToHide(0);
-        } else {
-            valueBar.setWeightToShow((float) percent);
-            valueBar.setWeightToHide((float) (1 - percent));
-        }
-
+        valueBar.setWeightToShow((float) percent);
         valueBar.setText((int) value + "/" + (int) valueMax);
         valueBar.setDescription(description);
         valueBar.setBarForegroundColor(color);
