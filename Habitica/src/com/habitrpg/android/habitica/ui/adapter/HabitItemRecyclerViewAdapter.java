HabitRPG/habitica-android

commit d6b6e897c10690e536431bf3ed885395b9e0209e

Force content loading on fragment launch

Habitica/src/com/habitrpg/android/habitica/ui/adapter/HabitItemRecyclerViewAdapter.java
@@ -73,7 +73,6 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
     Context context;
     public int dailyResetOffset;
 
-    private static final int TYPE_HEADER = 0;
     private static final int TYPE_CELL = 1;
     private RecyclerView.Adapter<ViewHolder> parentAdapter;
     private TagsHelper tagsHelper;
@@ -90,9 +89,9 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
         this.context = newContext;
         this.tagsHelper = tagsHelper;
         this.additionalEntries = additionalEntries;
-        filteredObservableContent = new ObservableArrayList<Task>();
+        filteredObservableContent = new ObservableArrayList<>();
 
-        this.loadContent();
+        this.loadContent(true);
 
         this.layoutResource = layoutResource;
         this.viewHolderClass = viewHolderClass;
@@ -246,11 +245,11 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
 
     // region ViewHolders
 
-    public abstract class ViewHolder<THabitItem extends Task> extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+    public abstract class ViewHolder<HabitItem extends Task> extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
         protected android.content.res.Resources resources;
 
-        public THabitItem Item;
+        public HabitItem Item;
 
         @Bind(R.id.notesTextView)
         TextView notesTextView;
@@ -269,7 +268,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
             resources = itemView.getResources();
         }
 
-        public void bindHolder(THabitItem habitItem, int position) {
+        public void bindHolder(HabitItem habitItem, int position) {
             Item = habitItem;
             if (habitItem.notes == null || habitItem.notes.length() == 0) {
                 notesTextView.setHeight(0);
@@ -407,7 +406,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
                 if (this.displayChecklist && this.Item.checklist != null) {
                     LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                     for (ChecklistItem item : this.Item.checklist) {
-                        LinearLayout itemView = (LinearLayout) layoutInflater.inflate(R.layout.checklist_item_row, null);
+                        LinearLayout itemView = (LinearLayout) layoutInflater.inflate(R.layout.checklist_item_row, this.checklistView);
                         CheckBox checkbox = (CheckBox) itemView.findViewById(R.id.checkBox);
                         TextView textView = (TextView) itemView.findViewById(R.id.checkedTextView);
                         // Populate the data into the template view using the data object
@@ -444,7 +443,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
                 while (v.getParent() != this.checklistView) {
                     v = (View) v.getParent();
                 }
-                Integer position = (Integer) ((ViewGroup) v.getParent()).indexOfChild(v);
+                Integer position = ((ViewGroup) v.getParent()).indexOfChild(v);
                 if (Item.checklist.size() > position && isChecked != Item.checklist.get(position).getCompleted()) {
                     TaskSaveEvent event = new TaskSaveEvent();
                     Item.checklist.get(position).setCompleted(isChecked);
@@ -555,7 +554,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
             return new MaterialDialog.Builder(context)
                     .onPositive(new MaterialDialog.SingleButtonCallback() {
                         @Override
-                        public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
+                        public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
                             BuyRewardCommand event = new BuyRewardCommand();
                             event.Reward = Item;
                             EventBus.getDefault().post(event);
@@ -569,7 +568,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
                     .negativeText(R.string.reward_dialog_dismiss)
                     .onNegative(new MaterialDialog.SingleButtonCallback() {
                         @Override
-                        public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
+                        public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
                             materialDialog.dismiss();
                         }
                     }).build();
@@ -651,10 +650,6 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
     }
 
     // endregion
-
-    public void loadContent() {
-        this.loadContent(false);
-    }
 
     public void loadContent(boolean forced) {
 

Habitica/src/com/habitrpg/android/habitica/ui/activities/LoginActivity.java
@@ -222,10 +222,10 @@ public class LoginActivity extends AppCompatActivity
 
     private void toggleRegistering() {
         this.isRegistering = !this.isRegistering;
-        this.setRegistering(this.isRegistering);
+        this.setRegistering();
 	}
 
-	private void setRegistering(boolean registering) {
+	private void setRegistering() {
         MenuItem menuItem = menu.findItem(R.id.action_toggleRegistering);
         if (this.isRegistering) {
             this.mLoginNormalBtn.setText(getString(R.string.register_btn));
@@ -256,7 +256,7 @@ public class LoginActivity extends AppCompatActivity
 	}
 
 	private void parse(String contents) {
-		String adr=null,user=null,key=null;
+		String adr,user,key;
 		try {
 			JSONObject obj;
 

Habitica/src/com/habitrpg/android/habitica/ui/activities/MainActivity.java
@@ -158,7 +158,12 @@ public class MainActivity extends BaseActivity implements HabitRPGUserCallback.O
 
         setupCheckout();
         EventBus.getDefault().register(this);
-        mAPIHelper.retrieveUser(new HabitRPGUserCallback(this));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     private void setupCheckout() {
@@ -209,11 +214,11 @@ public class MainActivity extends BaseActivity implements HabitRPGUserCallback.O
     private TransactionListener<HabitRPGUser> userTransactionListener = new TransactionListener<HabitRPGUser>() {
         @Override
         public void onResultReceived(HabitRPGUser habitRPGUser) {
-            user = habitRPGUser;
+            MainActivity.this.user = habitRPGUser;
             if (activeFragment == null) {
-                drawer.setSelectionAtPosition(1);
+                MainActivity.this.drawer.setSelectionAtPosition(1);
             }
-            setUserData(true);
+            MainActivity.this.setUserData(true);
         }
 
         @Override
@@ -298,7 +303,7 @@ public class MainActivity extends BaseActivity implements HabitRPGUserCallback.O
                     @Override
                     public void onResultReceived(List<Task> tasks) {
 
-                        ArrayList<Task> tasksToDelete = new ArrayList<Task>();
+                        ArrayList<Task> tasksToDelete = new ArrayList<>();
 
                         for (Task dbTask : tasks) {
                             if (!onlineTaskIdList.contains(dbTask.getId())) {
@@ -523,8 +528,6 @@ public class MainActivity extends BaseActivity implements HabitRPGUserCallback.O
                 public void success(Void aVoid, Response response) {
                     if (!event.Reward.getId().equals("potion")) {
                         EventBus.getDefault().post(new TaskRemovedEvent(event.Reward.getId()));
-                    } else {
-                        // TODO Update gears in avatar
                     }
 
                     user.async().save();

Habitica/src/com/habitrpg/android/habitica/ui/activities/PrefsActivity.java
@@ -41,21 +41,9 @@ public class PrefsActivity extends AppCompatActivity {
         HostConfig config;
         String httpPort = "80";
         String address = prefs.getString(ctx.getString(R.string.SP_address), ctx.getString(R.string.SP_address_default));
-        if (address.contains("http://habitrpg.com")) {
-            address = "https://habitrpg.com";
-            prefs.edit().putString(ctx.getString(R.string.SP_address), address).commit();
-        } else if (address.contains("http://beta.habitrpg.com")) {
-            address = "https://beta.habitrpg.com/";
-            prefs.edit().putString(ctx.getString(R.string.SP_address), address).commit();
-
-        }
-        if (address == null || address == "" || address.length() < 2) {
-            config = null;
-        } else {
-            String api = prefs.getString(ctx.getString(R.string.SP_APIToken), null);
-            String userID = prefs.getString(ctx.getString(R.string.SP_userID), null);
-            config = new HostConfig(address, httpPort, api, userID);
-        }
+        String api = prefs.getString(ctx.getString(R.string.SP_APIToken), null);
+        String userID = prefs.getString(ctx.getString(R.string.SP_userID), null);
+        config = new HostConfig(address, httpPort, api, userID);
         return config;
     }
 }

Habitica/src/com/habitrpg/android/habitica/ui/activities/SkillTasksActivity.java
@@ -19,9 +19,6 @@ import java.util.Map;
 
 import butterknife.Bind;
 
-/**
- * Created by viirus on 28/11/15.
- */
 public class SkillTasksActivity extends BaseActivity {
 
     @Bind(R.id.viewpager)

Habitica/src/com/habitrpg/android/habitica/ui/activities/TaskFormActivity.java
@@ -48,7 +48,6 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
 import de.greenrobot.event.EventBus;
 
 
@@ -251,7 +250,6 @@ public class TaskFormActivity extends BaseActivity implements AdapterView.OnItem
 
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(checklistAdapter);
-        int i = checklistAdapter.getItemCount();
 
         recyclerView.setLayoutManager(new WrapContentRecyclerViewLayoutManager(this));
 
@@ -305,7 +303,7 @@ public class TaskFormActivity extends BaseActivity implements AdapterView.OnItem
         if (this.dailyFrequencySpinner.getSelectedItemPosition() == 0) {
             String[] weekdays = getResources().getStringArray(R.array.weekdays);
             for (int i = 0; i < 7; i++) {
-                View weekdayRow = getLayoutInflater().inflate(R.layout.row_checklist, null);
+                View weekdayRow = getLayoutInflater().inflate(R.layout.row_checklist, this.frequencyContainer);
                 TextView tv = (TextView) weekdayRow.findViewById(R.id.label);
                 CheckBox checkbox = (CheckBox) weekdayRow.findViewById(R.id.checkbox);
                 checkbox.setChecked(true);
@@ -314,7 +312,7 @@ public class TaskFormActivity extends BaseActivity implements AdapterView.OnItem
                 this.frequencyContainer.addView(weekdayRow);
             }
         } else {
-            View dayRow = getLayoutInflater().inflate(R.layout.row_number_picker, null);
+            View dayRow = getLayoutInflater().inflate(R.layout.row_number_picker, this.frequencyContainer);
             this.frequencyPicker = (NumberPicker) dayRow.findViewById(R.id.numberPicker);
             this.frequencyPicker.setMinValue(1);
             this.frequencyPicker.setMaxValue(366);
@@ -455,8 +453,8 @@ public class TaskFormActivity extends BaseActivity implements AdapterView.OnItem
             case "daily": {
                 Calendar calendar = new GregorianCalendar();
                 calendar.set(startDatePicker.getYear(), startDatePicker.getMonth(), startDatePicker.getDayOfMonth());
-
                 task.setStartDate(new Date(calendar.getTimeInMillis()));
+
                 if (this.dailyFrequencySpinner.getSelectedItemPosition() == 0) {
                     task.setFrequency("weekly");
                     Days repeat = task.getRepeat();

Habitica/src/com/habitrpg/android/habitica/ui/fragments/BaseFragment.java
@@ -18,9 +18,6 @@ import com.magicmicky.habitrpgwrapper.lib.models.HabitRPGUser;
 import de.greenrobot.event.EventBus;
 import de.greenrobot.event.EventBusException;
 
-/**
- * Created by admin on 18/11/15.
- */
 public abstract class BaseFragment extends Fragment {
 
     private boolean registerEventBus = false;

Habitica/src/com/habitrpg/android/habitica/ui/fragments/TasksFragment.java
@@ -209,8 +209,6 @@ public class TasksFragment extends BaseFragment implements OnCheckedChangeListen
 
         viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
 
-            int oldPosition = -1;
-
             @Override
             public Fragment getItem(int position) {
                 int layoutOfType;
@@ -248,7 +246,7 @@ public class TasksFragment extends BaseFragment implements OnCheckedChangeListen
                                                 public void success(List<ItemData> itemDatas, Response response) {
 
                                                     // get itemdata list
-                                                    ArrayList<String> itemKeys = new ArrayList<String>();
+                                                    ArrayList<String> itemKeys = new ArrayList<>();
                                                     for (ItemData item : itemDatas) {
                                                         itemKeys.add(item.key);
                                                     }
@@ -257,7 +255,7 @@ public class TasksFragment extends BaseFragment implements OnCheckedChangeListen
                                                     contentCache.GetItemDataList(itemKeys, new ContentCache.GotContentEntryCallback<List<ItemData>>() {
                                                         @Override
                                                         public void GotObject(List<ItemData> obj) {
-                                                            ArrayList<Task> buyableItems = new ArrayList<Task>();
+                                                            ArrayList<Task> buyableItems = new ArrayList<>();
 
                                                             for (ItemData item : obj) {
                                                                 Task reward = new Task();
@@ -337,6 +335,7 @@ public class TasksFragment extends BaseFragment implements OnCheckedChangeListen
         }
         if (this.user != null) {
             fillTagFilterDrawer(user.getTags());
+
             for (TaskRecyclerViewFragment fragm : ViewFragmentsDictionary.values()) {
                 if (fragm != null) {
                     final HabitItemRecyclerViewAdapter adapter = (HabitItemRecyclerViewAdapter) fragm.mAdapter;
@@ -358,7 +357,7 @@ public class TasksFragment extends BaseFragment implements OnCheckedChangeListen
         }
         Bundle bundle = new Bundle();
         bundle.putString("type", type);
-        bundle.putStringArrayList("tagsId", new ArrayList<String>(this.tagsHelper.getTags()));
+        bundle.putStringArrayList("tagsId", new ArrayList<>(this.tagsHelper.getTags()));
 
         Intent intent = new Intent(activity, TaskFormActivity.class);
         intent.putExtras(bundle);

commit 6ee59fffe63d3b35095ed2a2ca26ef2c67b2bbb7

Fixes a checklist crash introduced by d6b6e897c.
Crash observed when expanding a checklist:

```
java.lang.IllegalStateException: The specified child already has a parent. You must call removeView() on the child's parent first.
	at android.view.ViewGroup.addViewInner(ViewGroup.java:4312)
	at android.view.ViewGroup.addView(ViewGroup.java:4148)
	at android.view.ViewGroup.addView(ViewGroup.java:4089)
	at android.view.ViewGroup.addView(ViewGroup.java:4062)
	at com.habitrpg.android.habitica.ui.adapter.HabitItemRecyclerViewAdapter$ChecklistedViewHolder.setDisplayChecklist(HabitItemRecyclerViewAdapter.java:418)
	at com.habitrpg.android.habitica.ui.adapter.HabitItemRecyclerViewAdapter$TodoViewHolder.setDisplayChecklist(HabitItemRecyclerViewAdapter.java:514)
	at com.habitrpg.android.habitica.ui.adapter.HabitItemRecyclerViewAdapter$ChecklistedViewHolder.onClick(HabitItemRecyclerViewAdapter.java:378)
	at android.view.View.performClick(View.java:5201)
	at android.view.View$PerformClick.run(View.java:21163)
	at android.os.Handler.handleCallback(Handler.java:746)
	at android.os.Handler.dispatchMessage(Handler.java:95)
	at android.os.Looper.loop(Looper.java:148)
	at android.app.ActivityThread.main(ActivityThread.java:5443)
	at java.lang.reflect.Method.invoke(Native Method)
	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:728)
	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:618)
```

This is happening because d6b6e897c adds a parent when inflating
`itemView`. The default behavior is to attach the inflated view to its
parent, which conflicts with the later line explicitly adding the view.

Habitica/src/com/habitrpg/android/habitica/ui/adapter/HabitItemRecyclerViewAdapter.java
@@ -406,7 +406,7 @@ public class HabitItemRecyclerViewAdapter<THabitItem extends Task>
                 if (this.displayChecklist && this.Item.checklist != null) {
                     LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                     for (ChecklistItem item : this.Item.checklist) {
-                        LinearLayout itemView = (LinearLayout) layoutInflater.inflate(R.layout.checklist_item_row, this.checklistView);
+                        LinearLayout itemView = (LinearLayout) layoutInflater.inflate(R.layout.checklist_item_row, this.checklistView, false);
                         CheckBox checkbox = (CheckBox) itemView.findViewById(R.id.checkBox);
                         TextView textView = (TextView) itemView.findViewById(R.id.checkedTextView);
                         // Populate the data into the template view using the data object