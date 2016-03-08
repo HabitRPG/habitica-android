d37c0e128fb49bffa7429e6a391df1193c476f8e
Phillip Thelen <viirus@pherth.net>
2016-03-08 15:41:00 +0100

    fix login setup

--- Habitica/src/com/habitrpg/android/habitica/ui/activities/LoginActivity.java
+++ Habitica/src/com/habitrpg/android/habitica/ui/activities/LoginActivity.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
