415d15c6d28dd062ece51075a3f2e9f196f09540
Saran Lertpradit <saranlert@users.noreply.github.com>
2016-03-17 07:27:42 +0700

    update buyResponse

--- Habitica/src/com/magicmicky/habitrpgwrapper/lib/models/responses/BuyResponse.java
+++ Habitica/src/com/magicmicky/habitrpgwrapper/lib/models/responses/BuyResponse.java
@@ -1,9 +1,18 @@
 package com.magicmicky.habitrpgwrapper.lib.models.responses;
 
-import com.magicmicky.habitrpgwrapper.lib.models.HabitRPGUser;
+import com.magicmicky.habitrpgwrapper.lib.models.Flags;
+import com.magicmicky.habitrpgwrapper.lib.models.Items;
+import com.magicmicky.habitrpgwrapper.lib.models.Stats;
 
 import java.util.HashMap;
 
-public class BuyResponse extends HabitRPGUser {
+public class BuyResponse {
+
+    public Items items;
+
+    public Stats stats;
+
+    public Flags flags;
+
     public HashMap<String, String> armoire;
 }
