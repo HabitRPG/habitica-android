d47cd8a9074ce70811862f14bb962df824aa9c75
Phillip Thelen <viirus@pherth.net>
2016-04-14 14:29:42 +0200

    fix db upgrade

--- Habitica/assets/migrations/Habitica/18.sql
+++ Habitica/assets/migrations/Habitica/18.sql
@@ -1,8 +1,1 @@
-ALTER TABLE Pet ADD COLUMN animalText varchar(255);
-ALTER TABLE Pet ADD COLUMN colorText varchar(255);
-ALTER TABLE Pet ADD COLUMN limited bool;
-ALTER TABLE Pet ADD COLUMN premium bool;
-ALTER TABLE Mount ADD COLUMN animalText varchar(255);
-ALTER TABLE Mount ADD COLUMN colorText varchar(255);
-ALTER TABLE Mount ADD COLUMN limited bool;
-ALTER TABLE Mount ADD COLUMN premium bool;
+ALTER TABLE QuestContent ADD COLUMN 'boss_id' bool;
