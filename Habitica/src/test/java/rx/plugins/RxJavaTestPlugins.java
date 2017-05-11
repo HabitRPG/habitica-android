581fb124d7f22959c1211e9c790ab6924a375319
Phillip Thelen <viirus@pherth.net>
2017-05-11 10:34:55 +0200

    fix test scheduler

---
+++ Habitica/src/test/java/rx/plugins/RxJavaTestPlugins.java
@@ -0,0 +1,11 @@
+package rx.plugins;
+
+public class RxJavaTestPlugins extends RxJavaPlugins {
+    RxJavaTestPlugins() {
+        super();
+    }
+
+    public static void resetPlugins(){
+        getInstance().reset();
+    }
+}
