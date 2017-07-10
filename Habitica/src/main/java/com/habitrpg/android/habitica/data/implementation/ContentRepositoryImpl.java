72158627945d3663204dbcc465431c51cdc85f3a
Phillip Thelen <viirus@pherth.net>
2017-07-10 19:11:23 +0200

    rate limit content loading

--- Habitica/src/main/java/com/habitrpg/android/habitica/data/implementation/ContentRepositoryImpl.java
+++ Habitica/src/main/java/com/habitrpg/android/habitica/data/implementation/ContentRepositoryImpl.java
@@ -6,9 +6,17 @@ import com.habitrpg.android.habitica.data.local.BaseLocalRepository;
 import com.habitrpg.android.habitica.data.local.ContentLocalRepository;
 import com.habitrpg.android.habitica.models.ContentResult;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 
 abstract class ContentRepositoryImpl<T extends ContentLocalRepository> extends BaseRepositoryImpl<T> implements ContentRepository {
+
+    private Date lastSync = null;
 
     public ContentRepositoryImpl(T localRepository, ApiClient apiClient) {
         super(localRepository, apiClient);
@@ -16,8 +24,19 @@ abstract class ContentRepositoryImpl<T extends ContentLocalRepository> extends B
 
     @Override
     public Observable<ContentResult> retrieveContent() {
-        return apiClient.getContent()
-                .doOnNext(localRepository::saveContent);
+        return retrieveContent(false);
+    }
+
+    @Override
+    public Observable<ContentResult> retrieveContent(boolean forced) {
+        if (forced || this.lastSync == null || (new Date().getTime() - this.lastSync.getTime()) > 3600000) {
+            lastSync = new Date();
+            return apiClient.getContent()
+                    .doOnNext(localRepository::saveContent);
+        } else {
+            return Observable.just(null);
+        }
+
     }
 
 }
