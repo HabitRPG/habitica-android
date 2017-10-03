1a121ded2f8e1524268e1a4c71d6907ce22fccd1
Phillip Thelen <viirus@pherth.net>
2017-10-03 20:47:56 +0200

    fix error when trying to show dialog without window

---
+++ Habitica/src/main/java/com/habitrpg/android/habitica/events/ShowCheckinDialog.java
@@ -0,0 +1,8 @@
+package com.habitrpg.android.habitica.events;
+
+import com.habitrpg.android.habitica.models.Notification;
+
+public class ShowCheckinDialog {
+    public Notification notification;
+    public String nextUnlockText;
+}
--- Habitica/src/main/java/com/habitrpg/android/habitica/helpers/PopupNotificationsManager.java
+++ Habitica/src/main/java/com/habitrpg/android/habitica/helpers/PopupNotificationsManager.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.habitrpg.android.habitica.R;
 import com.habitrpg.android.habitica.data.ApiClient;
+import com.habitrpg.android.habitica.events.ShowCheckinDialog;
 import com.habitrpg.android.habitica.events.ShowSnackbarEvent;
 import com.habitrpg.android.habitica.models.Notification;
 import com.habitrpg.android.habitica.ui.helpers.DataBindingUtils;
@@ -50,51 +51,10 @@ public class PopupNotificationsManager {
     Boolean displayNotification(Notification notification) {
         String nextUnlockText = context.getString(R.string.nextPrizeUnlocks, notification.data.nextRewardAt);
         if (notification.data.rewardKey != null) {
-            String title = notification.data.message;
-
-            LayoutInflater factory = LayoutInflater.from(context);
-            final View view = factory.inflate(R.layout.dialog_login_incentive, null);
-
-            SimpleDraweeView imageView = (SimpleDraweeView) view.findViewById(R.id.imageView);
-            String imageKey = notification.data.rewardKey.get(0);
-            DataBindingUtils.loadImage(imageView, imageKey);
-
-            String youEarnedMessage = context.getString(R.string.checkInRewardEarned, notification.data.rewardText);
-
-            TextView titleTextView = new TextView(context);
-            titleTextView.setBackgroundResource(R.color.blue_100);
-            titleTextView.setTextColor(ContextCompat.getColor(context, R.color.white));
-            float density = context.getResources().getDisplayMetrics().density;
-            int paddingDp = (int) (16 * density);
-            titleTextView.setPadding(paddingDp, paddingDp, paddingDp, paddingDp);
-            titleTextView.setTextSize(18);
-            titleTextView.setGravity(Gravity.CENTER_HORIZONTAL);
-            titleTextView.setText(title);
-
-            TextView youEarnedTexView = (TextView) view.findViewById(R.id.you_earned_message);
-            youEarnedTexView.setText(youEarnedMessage);
-
-            TextView nextUnlockTextView = (TextView) view.findViewById(R.id.next_unlock_message);
-            nextUnlockTextView.setText(nextUnlockText);
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AlertDialogTheme)
-                    .setView(view)
-                    .setCustomTitle(titleTextView)
-                    .setPositiveButton(R.string.start_day, (dialog, which) -> {
-                        if (apiClient != null) {
-                            // @TODO: This should be handled somewhere else? MAybe we notifiy via event
-                            apiClient.readNotification(notification.getId())
-                                    .subscribe(next -> {}, RxErrorHandler.handleEmptyError());
-                        }
-                    })
-                    .setMessage("");
-
-            Observable.just(null)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(o -> {
-                        final AlertDialog dialog = builder.create();
-                        dialog.show();
-                    }, RxErrorHandler.handleEmptyError());
+            ShowCheckinDialog event = new ShowCheckinDialog();
+            event.notification = notification;
+            event.nextUnlockText = nextUnlockText;
+            EventBus.getDefault().post(event);
         } else {
             ShowSnackbarEvent event = new ShowSnackbarEvent();
             event.title = notification.data.message;
