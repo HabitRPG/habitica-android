41072d1caec9811eeae9a25a9198180f0c6e69f4
Phillip Thelen <viirus@pherth.net>
2017-11-14 17:35:16 +0100

    show stat points on correct menu item. Fixes #869

--- Habitica/src/main/java/com/habitrpg/android/habitica/extensions/ViewExt.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/extensions/ViewExt.kt
@@ -1,9 +1,15 @@
 package com.habitrpg.android.habitica.extensions
 
 import android.content.Context
+import android.support.annotation.IdRes
 import android.view.View
 
 
-public fun View.setScaledPadding(context: Context?, left: Int, top: Int, right: Int, bottom: Int) {
+fun View.setScaledPadding(context: Context?, left: Int, top: Int, right: Int, bottom: Int) {
     this.setPadding(left.dpToPx(context), top.dpToPx(context), right.dpToPx(context), bottom.dpToPx(context))
+}
+
+fun <T : View> View.bindView(@IdRes res: Int) : Lazy<T> {
+    @Suppress("UNCHECKED_CAST")
+    return lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(res) }
 }
