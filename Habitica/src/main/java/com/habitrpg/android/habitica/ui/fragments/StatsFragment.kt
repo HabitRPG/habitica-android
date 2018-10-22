48253d1c2fb3749613521883d619c3dfa51e3e39
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-10-22 20:48:32 +0300

    Fix buffs in Stats view

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/StatsFragment.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/StatsFragment.kt
@@ -220,14 +220,14 @@ class StatsFragment: BaseMainFragment() {
         constitutionStatsView.levelValue = levelStat
         perceptionStatsView.levelValue = levelStat
 
-        totalStrength += currentUser.stats?.buffs?.str?.toInt() ?: 0
-        totalIntelligence += currentUser.stats?.buffs?._int?.toInt() ?: 0
-        totalConstitution += currentUser.stats?.buffs?.con?.toInt() ?: 0
-        totalPerception += currentUser.stats?.buffs?.per?.toInt() ?: 0
-        strengthStatsView.buffValue = currentUser.stats?.buffs?.str?.toInt() ?: 0
-        intelligenceStatsView.buffValue = currentUser.stats?.buffs?._int?.toInt() ?: 0
-        constitutionStatsView.buffValue = currentUser.stats?.buffs?.con?.toInt() ?: 0
-        perceptionStatsView.buffValue = currentUser.stats?.buffs?.per?.toInt() ?: 0
+        totalStrength += currentUser.stats?.buffs?.getStr()?.toInt() ?: 0
+        totalIntelligence += currentUser.stats?.buffs?.get_int()?.toInt() ?: 0
+        totalConstitution += currentUser.stats?.buffs?.getCon()?.toInt() ?: 0
+        totalPerception += currentUser.stats?.buffs?.getPer()?.toInt() ?: 0
+        strengthStatsView.buffValue = currentUser.stats?.buffs?.getStr()?.toInt() ?: 0
+        intelligenceStatsView.buffValue = currentUser.stats?.buffs?.get_int()?.toInt() ?: 0
+        constitutionStatsView.buffValue = currentUser.stats?.buffs?.getCon()?.toInt() ?: 0
+        perceptionStatsView.buffValue = currentUser.stats?.buffs?.getPer()?.toInt() ?: 0
 
         totalStrength += currentUser.stats?.str ?: 0
         totalIntelligence += currentUser.stats?._int ?: 0
