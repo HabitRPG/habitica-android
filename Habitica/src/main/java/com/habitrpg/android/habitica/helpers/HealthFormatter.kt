3a3fc44418c9944159ba587a11ab622279136cd1
Peter Treitler <peter.treitler@gmail.com>
2018-11-23 15:02:03 +0100

    HealthFormatter: Add formatToString function + tests

--- Habitica/src/main/java/com/habitrpg/android/habitica/helpers/HealthFormatter.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/helpers/HealthFormatter.kt
@@ -1,12 +1,33 @@
 package com.habitrpg.android.habitica.helpers
 
+import android.os.Build
+import java.text.NumberFormat
+import java.util.*
+
 object HealthFormatter {
     fun format(input: Int) = format(input.toDouble())
 
+    @JvmStatic
     fun format(input: Double) =
             if (input < 1 && input > 0) {
                 Math.ceil(input * 10) / 10
             } else {
                 Math.floor(input)
             }
+
+    fun formatToString(input: Int, locale: Locale = getDefaultLocale()) = formatToString(input.toDouble(), locale)
+
+    @JvmStatic
+    @JvmOverloads
+    fun formatToString(input: Double, locale: Locale = getDefaultLocale()): String {
+        val doubleValue = format(input)
+        val numberFormat = NumberFormat.getInstance(locale).apply { maximumFractionDigits = 1 }
+        return numberFormat.format(doubleValue)
+    }
+
+    private fun getDefaultLocale() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Locale.getDefault(Locale.Category.FORMAT)
+    } else {
+        Locale.getDefault()
+    }
 }
--- Habitica/src/test/java/com/habitrpg/android/habitica/helpers/HealthFormatterTest.kt
+++ Habitica/src/test/java/com/habitrpg/android/habitica/helpers/HealthFormatterTest.kt
@@ -2,6 +2,7 @@ package com.habitrpg.android.habitica.helpers
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class HealthFormatterTest {
     @Test
@@ -10,6 +11,11 @@ class HealthFormatterTest {
         assertEquals(9.0, HealthFormatter.format(9.9999), DELTA)
         assertEquals(1.0, HealthFormatter.format(1.9), DELTA)
         assertEquals(1.0, HealthFormatter.format(1.0001), DELTA)
+
+        assertEquals("49", HealthFormatter.formatToString(49.9, Locale.US))
+        assertEquals("9", HealthFormatter.formatToString(9.9999, Locale.US))
+        assertEquals("1", HealthFormatter.formatToString(1.9, Locale.US))
+        assertEquals("1", HealthFormatter.formatToString(1.0001, Locale.US))
     }
 
     @Test
@@ -17,12 +23,19 @@ class HealthFormatterTest {
         assertEquals(1.0, HealthFormatter.format(0.99), DELTA)
         assertEquals(0.2, HealthFormatter.format(0.11), DELTA)
         assertEquals(0.1, HealthFormatter.format(0.0001), DELTA)
+
+        assertEquals("1", HealthFormatter.formatToString(0.99, Locale.US))
+        assertEquals("0.2", HealthFormatter.formatToString(0.11, Locale.US))
+        assertEquals("0.1", HealthFormatter.formatToString(0.0001, Locale.US))
     }
 
     @Test
     fun shouldRoundNegativeValuesDown() {
         assertEquals(-1.0, HealthFormatter.format(-0.1), DELTA)
         assertEquals(-2.0, HealthFormatter.format(-2.0), DELTA)
+
+        assertEquals("-1", HealthFormatter.formatToString(-0.1, Locale.US))
+        assertEquals("-2", HealthFormatter.formatToString(-2.0, Locale.US))
     }
 
     @Test
@@ -30,6 +43,10 @@ class HealthFormatterTest {
         assertEquals(20.0, HealthFormatter.format(20), DELTA)
         assertEquals(0.0, HealthFormatter.format(0), DELTA)
         assertEquals(0.9, HealthFormatter.format(0.9), DELTA)
+
+        assertEquals("20", HealthFormatter.formatToString(20, Locale.US))
+        assertEquals("0", HealthFormatter.formatToString(0, Locale.US))
+        assertEquals("0.9", HealthFormatter.formatToString(0.9, Locale.US))
     }
 
     companion object {
