0f86f44b3378675a3589b5eb0db7190399b060cf
Carl Vuorinen <carl.vuorinen@gmail.com>
2019-04-09 22:15:44 +0300

    Create notification item view and implement few notification types

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/NotificationsActivity.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/NotificationsActivity.kt
@@ -2,15 +2,17 @@ package com.habitrpg.android.habitica.ui.activities
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 import com.habitrpg.android.habitica.R
 import com.habitrpg.android.habitica.components.AppComponent
 import com.habitrpg.android.habitica.helpers.RxErrorHandler
-import com.habitrpg.android.habitica.models.notifications.GlobalNotification
+import com.habitrpg.android.habitica.models.notifications.*
 import com.habitrpg.android.habitica.ui.viewmodels.NotificationsViewModel
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_notifications.*
@@ -87,7 +89,10 @@ class NotificationsActivity : BaseActivity(), androidx.swiperefreshlayout.widget
 
         notifications.map {
             val item: View? = when (it.type) {
-                //TODO("not implemented")
+                NotificationType.NEW_CHAT_MESSAGE.type -> createNewChatMessageNotification(it)
+                NotificationType.NEW_STUFF.type -> createNewStuffNotification(it)
+                NotificationType.UNALLOCATED_STATS_POINTS.type -> createUnallocatedStatsNotification(it)
+                //TODO rest of the notification types
                 else -> null
             }
 
@@ -105,5 +110,62 @@ class NotificationsActivity : BaseActivity(), androidx.swiperefreshlayout.widget
         dismissAllButton?.setOnClickListener({ viewModel.dismissAllNotifications() })
 
         return header
+    }
+
+    private fun createNewChatMessageNotification(notification: GlobalNotification): View? {
+        val data = notification.getData() as? NewChatMessageData
+
+        return createNotificationItem(
+                notification,
+                fromHtml(getString(R.string.new_msg_guild, data?.group?.name))
+        )
+    }
+
+    private fun createNewStuffNotification(notification: GlobalNotification): View? {
+        val data = notification.getData() as? NewStuffData
+        val text = fromHtml("<b>" + getString(R.string.new_bailey_update) + "</b><br>" + data?.title)
+
+        return createNotificationItem(
+                notification,
+                text,
+                R.drawable.notifications_bailey
+        )
+    }
+
+    private fun createUnallocatedStatsNotification(notification: GlobalNotification): View? {
+        val data = notification.getData() as? UnallocatedPointsData
+
+        return createNotificationItem(
+                notification,
+                fromHtml(getString(R.string.unallocated_stats_points, data?.points.toString())),
+                R.drawable.notification_stat_sparkles
+        )
+    }
+
+    private fun createNotificationItem(notification: GlobalNotification, messageText: CharSequence, imageResourceId: Int? = null): View? {
+        val item = inflater.inflate(R.layout.notification_item, notification_items, false)
+
+        val dismissButton = item?.findViewById(R.id.dismiss_button) as? ImageView
+        dismissButton?.setOnClickListener({ viewModel.dismissNotification(notification) })
+
+        val messageTextView = item?.findViewById(R.id.message_text) as? TextView
+        messageTextView?.text = messageText
+
+        if (imageResourceId != null) {
+            val notificationImage = item?.findViewById(R.id.notification_image) as? ImageView
+            notificationImage?.setImageResource(imageResourceId)
+            notificationImage?.visibility = View.VISIBLE
+        }
+
+        return item
+    }
+
+    private fun fromHtml(text: String): CharSequence {
+        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(text)
+        }
     }
 }
