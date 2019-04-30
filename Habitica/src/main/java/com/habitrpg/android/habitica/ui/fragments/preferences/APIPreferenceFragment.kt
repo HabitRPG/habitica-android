13ee4fe7acb14a5e2103a61b12f69279368fb5c3
Phillip Thelen <phillip@habitica.com>
2019-04-30 13:39:08 +0200

    remove QR Code dependency

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/preferences/APIPreferenceFragment.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/preferences/APIPreferenceFragment.kt
@@ -4,37 +4,26 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import androidx.preference.Preference
 import android.widget.Toast
+import androidx.preference.Preference
 import com.habitrpg.android.habitica.HabiticaBaseApplication
 import com.habitrpg.android.habitica.R
-import com.habitrpg.android.habitica.helpers.QrCodeManager
 import java.util.*
 
 class APIPreferenceFragment: BasePreferencesFragment() {
     private val apiPreferences: List<String>
         get() = Arrays.asList(getString(R.string.SP_APIToken), getString(R.string.SP_userID))
 
-    private lateinit var qrCodeManager: QrCodeManager
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         HabiticaBaseApplication.component?.inject(this)
-
-        qrCodeManager = QrCodeManager(userRepository, this.context)
-
         super.onCreate(savedInstanceState)
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        when (preference.key) {
-            "SP_user_qr_code" -> qrCodeManager.showDialogue()
-            else -> {
-                val clipMan = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                clipMan.primaryClip = ClipData.newPlainText(preference.key, preference.summary)
-                Toast.makeText(activity, "Copied " + preference.key + " to clipboard.", Toast.LENGTH_SHORT).show()
-            }
-        }
+        val clipMan = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+        clipMan?.primaryClip = ClipData.newPlainText(preference.key, preference.summary)
+        Toast.makeText(activity, "Copied " + preference.key + " to clipboard.", Toast.LENGTH_SHORT).show()
         return super.onPreferenceTreeClick(preference)
     }
 
