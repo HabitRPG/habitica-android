6107b971364c87bc60557e0e434e7bc07ca19d14
Phillip Thelen <phillip@habitica.com>
2019-11-01 18:06:43 +0100

    fix navigation issue

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/SkillTasksActivity.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/SkillTasksActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.SparseArray
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
@@ -90,6 +91,16 @@ class SkillTasksActivity : BaseActivity() {
         resultIntent.putExtra("taskID", task.id)
         setResult(Activity.RESULT_OK, resultIntent)
         finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
