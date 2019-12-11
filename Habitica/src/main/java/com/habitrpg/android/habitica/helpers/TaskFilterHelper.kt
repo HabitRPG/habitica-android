5181fe29353ce4008e87073aa286b98ae964e333
violetta0012 <37185898+violetta0012@users.noreply.github.com>
2019-12-11 17:22:55 +0300

    Filtered Todo's are sorted by date (#1241)
    * Filtered Todo's are sorted by date
    
    * tried to fix

--- Habitica/src/main/java/com/habitrpg/android/habitica/helpers/TaskFilterHelper.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/helpers/TaskFilterHelper.kt
@@ -111,7 +111,7 @@ class TaskFilterHelper {
                     Task.FILTER_GRAY -> query = query.equalTo("completed", true).or().equalTo("isDue", false)
                     Task.FILTER_WEAK -> query = query.lessThan("value", 1.0)
                     Task.FILTER_STRONG -> query = query.greaterThanOrEqualTo("value", 1.0)
-                    Task.FILTER_DATED -> query = query.isNotNull("dueDate").equalTo("completed", false)
+                    Task.FILTER_DATED -> query = query.isNotNull("dueDate").equalTo("completed", false).sortBy("dueDate")
                     Task.FILTER_COMPLETED -> query = query.equalTo("completed", true)
                 }
             }
