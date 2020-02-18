c68e4ff12a3411a8100e6f40df47a99fb83e261f
Phillip Thelen <phillip@habitica.com>
2020-02-18 12:07:47 +0100

    Small changes to setup process

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/setup/IntroFragment.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/setup/IntroFragment.kt
@@ -5,24 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import com.habitrpg.android.habitica.R
 import com.habitrpg.android.habitica.components.UserComponent
-import com.habitrpg.android.habitica.extensions.inflate
+import com.habitrpg.android.habitica.databinding.FragmentIntroBinding
 import com.habitrpg.android.habitica.ui.fragments.BaseFragment
-import com.habitrpg.android.habitica.ui.helpers.bindOptionalView
-import com.habitrpg.android.habitica.ui.helpers.resetViews
 
 class IntroFragment : BaseFragment() {
 
-    private val subtitleTextView: TextView? by bindOptionalView(R.id.subtitleTextView)
-    private val titleTextView: TextView? by bindOptionalView(R.id.titleTextView)
-    private val titleImageView: ImageView? by bindOptionalView(R.id.titleImageView)
-    private val descriptionTextView: TextView? by bindOptionalView(R.id.descriptionTextView)
-    private val imageView: ImageView? by bindOptionalView(R.id.imageView)
-    private val containerView: ViewGroup? by bindOptionalView(R.id.container_view)
-
+    private var binding: FragmentIntroBinding? = null
     private var image: Drawable? = null
     private var titleImage: Drawable? = null
     private var subtitle: String? = null
@@ -32,36 +21,35 @@ class IntroFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return container?.inflate(R.layout.fragment_intro)
+        binding = FragmentIntroBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        resetViews()
-
         if (this.image != null) {
-            this.imageView?.setImageDrawable(this.image)
+            binding?.imageView?.setImageDrawable(this.image)
         }
 
         if (this.titleImage != null) {
-            this.titleImageView?.setImageDrawable(this.titleImage)
+            binding?.titleImageView?.setImageDrawable(this.titleImage)
         }
 
         if (this.subtitle != null) {
-            this.subtitleTextView?.text = this.subtitle
+            binding?.subtitleTextView?.text = this.subtitle
         }
 
         if (this.title != null) {
-            this.titleTextView?.text = this.title
+            binding?.titleTextView?.text = this.title
         }
 
         if (this.description != null) {
-            this.descriptionTextView?.text = this.description
+            binding?.descriptionTextView?.text = this.description
         }
 
         backgroundColor?.let {
-            this.containerView?.setBackgroundColor(it)
+            binding?.containerView?.setBackgroundColor(it)
         }
     }
 
@@ -72,33 +60,33 @@ class IntroFragment : BaseFragment() {
     fun setImage(image: Drawable?) {
         this.image = image
         if (image != null) {
-            this.imageView?.setImageDrawable(image)
+            binding?.imageView?.setImageDrawable(image)
         }
     }
 
     fun setTitleImage(image: Drawable?) {
         this.titleImage = image
-        this.titleImageView?.setImageDrawable(image)
+        binding?.titleImageView?.setImageDrawable(image)
     }
 
     fun setSubtitle(text: String?) {
         this.subtitle = text
-        subtitleTextView?.text = text
+        binding?.subtitleTextView?.text = text
     }
 
     fun setTitle(text: String?) {
         this.title = text
-        titleTextView?.text = text
+        binding?.titleTextView?.text = text
     }
 
     fun setDescription(text: String?) {
         this.description = text
-        descriptionTextView?.text = text
+        binding?.descriptionTextView?.text = text
     }
 
     fun setBackgroundColor(color: Int) {
         this.backgroundColor = color
-        containerView?.setBackgroundColor(color)
+        binding?.containerView?.setBackgroundColor(color)
     }
 
 }
