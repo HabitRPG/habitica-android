d136d5e14be59e0d8b1568335a7c619bf0d6d7ef
Leland Wu <leland.wu4@gmail.com>
2020-03-03 16:33:23 -0500

    rewrite if else as kotlin when

--- Habitica/src/main/java/com/habitrpg/android/habitica/models/inventory/AnimalExtension.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/models/inventory/AnimalExtension.kt
@@ -4,19 +4,15 @@ import android.content.Context
 import com.habitrpg.android.habitica.R
 
 fun Animal.getTranslatedType(c: Context?): String {
-    var currType = type
 
-    if (currType == "drop") {
-        currType = c?.getString(R.string.standard)
-    }
-    if (currType == "quest") {
-        currType = c?.getString(R.string.quest)
-    }
-    if (currType == "wacky") {
-        currType = c?.getString(R.string.wacky)
-    }
-    if (currType == "special") {
-        currType = c?.getString(R.string.special)
+    var currType: String = when (type) {
+        "drop"    -> c?.getString(R.string.standard).toString()
+        "quest"   -> c?.getString(R.string.quest).toString()
+        "wacky"   -> c?.getString(R.string.wacky).toString()
+        "special" -> c?.getString(R.string.special).toString()
+        else      -> {
+            type
+        }
     }
 
     return currType
