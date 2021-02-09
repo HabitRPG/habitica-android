089d05050e78ee0ccf17dfd766bd58f998364095
Isabel Lally <ilally93@gmail.com>
2021-02-09 03:06:56 -0500

    timestamp works slightly

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/social/ChatRecyclerViewAdapter.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/social/ChatRecyclerViewAdapter.kt
@@ -1,8 +1,10 @@
 package com.habitrpg.android.habitica.ui.adapter.social
 
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.habitrpg.android.habitica.R
 import com.habitrpg.android.habitica.extensions.inflate
@@ -14,6 +16,8 @@ import com.habitrpg.android.habitica.ui.viewHolders.ChatRecyclerMessageViewHolde
 import io.reactivex.rxjava3.core.BackpressureStrategy
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.subjects.PublishSubject
+import java.time.Instant
+import java.time.ZoneId
 
 class ChatRecyclerViewAdapter(user: User?, private val isTavern: Boolean) : BaseRecyclerViewAdapter<ChatMessage, RecyclerView.ViewHolder>() {
     internal var user = user
@@ -45,7 +49,10 @@ class ChatRecyclerViewAdapter(user: User?, private val isTavern: Boolean) : Base
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (data[position].isSystemMessage) {
-            (holder as? SystemChatMessageViewHolder)?.bind(data[position])
+            val sysChatHolder = holder as? SystemChatMessageViewHolder ?: return
+            sysChatHolder.bind(data[position],
+                    expandedMessageId == data[position].id)
+            sysChatHolder.onShouldExpand = { expandMessage(data[position].id, position)}
         } else {
             val chatHolder = holder as? ChatRecyclerMessageViewHolder ?: return
             val message = data[position]
@@ -103,9 +110,22 @@ class ChatRecyclerViewAdapter(user: User?, private val isTavern: Boolean) : Base
 
 class SystemChatMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val textView: TextView = itemView.findViewById(R.id.text_view)
+    private val timestamp: TextView = itemView.findViewById(R.id.system_message_timestamp)
+    private val dateTime = java.text.SimpleDateFormat("MMM dd, hh:mm aaa")
 
-    fun bind(chatMessage: ChatMessage?) {
+    var onShouldExpand: (() -> Unit)? = null
+
+    init{
+        textView.setOnClickListener {
+            onShouldExpand?.invoke()
+        }
+    }
+
+    fun bind(chatMessage: ChatMessage?, isExpanded: Boolean) {
         textView.text = chatMessage?.text?.removePrefix("`")?.removeSuffix("`")
+        if(isExpanded) {
+            timestamp.text = dateTime.format(chatMessage?.timestamp?.let { java.util.Date(it) })
+            }
     }
 
 }
