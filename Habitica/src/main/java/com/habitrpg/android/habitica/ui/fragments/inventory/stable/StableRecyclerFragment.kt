3d33d5f57d2a09b011fe0cf2fce0a234163347b6
pauliancu97 <paul97iancu@gmail.com>
2021-11-06 14:21:18 +0200

    remove special pets/mounts section if the user does not own any special pets/mounts

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/inventory/stable/StableRecyclerFragment.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/inventory/stable/StableRecyclerFragment.kt
@@ -258,6 +258,7 @@ class StableRecyclerFragment :
         }
 
         items.add(0, "header")
+        items.removeAll { it is StableSection && (it.key as? String) == "special" && it.ownedCount == 0}
         return items
     }
 
