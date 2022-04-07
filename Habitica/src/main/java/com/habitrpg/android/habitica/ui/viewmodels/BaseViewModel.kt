90b439aedd61d27ed4fd43de89f09ddecde963f6
Hafiz <hafizbhamidi@gmail.com>
2022-04-07 01:52:11 -0400

    Remove duplicate observeOnce

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/viewmodels/BaseViewModel.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/viewmodels/BaseViewModel.kt
@@ -54,13 +54,4 @@ abstract class BaseViewModel(initializeComponent: Boolean = true) : ViewModel() 
         disposable.add(userRepository.updateUser(path, value)
             .subscribe({ }, RxErrorHandler.handleEmptyError()))
     }
-
-    fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
-        observe(lifecycleOwner, object : Observer<T> {
-            override fun onChanged(t: T?) {
-                observer.onChanged(t)
-                removeObserver(this)
-            }
-        })
-    }
 }
