c3240be6d8a23c821653cbce5214e3dfbe01b230
Hafiz <hafizbhamidi@gmail.com>
2022-04-26 04:15:33 -0400

    Create Account Dialog fragment for resetting and deleting account

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/preferences/AccountPreferenceFragment.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/preferences/AccountPreferenceFragment.kt
@@ -7,10 +7,8 @@ import android.content.ClipboardManager
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.text.InputType
 import android.view.View
 import android.widget.EditText
-import android.widget.LinearLayout
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
@@ -28,6 +26,7 @@ import com.habitrpg.android.habitica.extensions.layoutInflater
 import com.habitrpg.android.habitica.helpers.RxErrorHandler
 import com.habitrpg.android.habitica.models.user.User
 import com.habitrpg.android.habitica.ui.activities.FixCharacterValuesActivity
+import com.habitrpg.android.habitica.ui.fragments.preferences.HabiticaAccountDialog.AccountUpdateConfirmed
 import com.habitrpg.android.habitica.ui.helpers.KeyboardUtil
 import com.habitrpg.android.habitica.ui.viewmodels.AuthenticationViewModel
 import com.habitrpg.android.habitica.ui.views.ExtraLabelPreference
@@ -40,13 +39,15 @@ import javax.inject.Inject
 
 class AccountPreferenceFragment :
     BasePreferencesFragment(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
+    SharedPreferences.OnSharedPreferenceChangeListener,
+        AccountUpdateConfirmed {
     @Inject
     lateinit var hostConfig: HostConfig
     @Inject
     lateinit var apiClient: ApiClient
 
     private lateinit var viewModel: AuthenticationViewModel
+    private lateinit var accountDialog: HabiticaAccountDialog
 
     override var user: User? = null
         set(value) {
@@ -410,28 +411,11 @@ class AccountPreferenceFragment :
     }
 
     private fun showAccountDeleteConfirmation() {
-        val view = context?.layoutInflater?.inflate(R.layout.dialog_edittext, null)
-        var deleteMessage = getString(R.string.delete_account_description)
-        val editText = view?.findViewById<EditText>(R.id.editText)
-        if (user?.authentication?.localAuthentication?.email != null) {
-            editText?.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-        } else {
-            deleteMessage = getString(R.string.delete_oauth_account_description)
-            editText?.inputType = InputType.TYPE_CLASS_TEXT
-        }
-        view?.findViewById<TextInputLayout>(R.id.input_layout)?.hint = context?.getString(R.string.confirm_deletion)
-        context?.let { context ->
-            val dialog = HabiticaAlertDialog(context)
-            dialog.setTitle(R.string.delete_account)
-            dialog.setMessage(deleteMessage)
-            dialog.addCancelButton()
-            dialog.addButton(R.string.delete, isPrimary = true, isDestructive = true) { _, _ ->
-                deleteAccount(editText?.text?.toString() ?: "")
-            }
-            dialog.setAdditionalContentView(view)
-            dialog.setAdditionalContentSidePadding(12.dpToPx(context))
-            dialog.buttonAxis = LinearLayout.HORIZONTAL
-            dialog.show()
+        val habiticaAccountDialog = context?.let { HabiticaAccountDialog(it, "delete_account", this) }
+        habiticaAccountDialog?.show(parentFragmentManager, "account")
+
+        if (habiticaAccountDialog != null) {
+            accountDialog = habiticaAccountDialog
         }
     }
 
@@ -440,6 +424,7 @@ class AccountPreferenceFragment :
         compositeSubscription.add(
             userRepository.deleteAccount(password).subscribe({ _ ->
                 dialog?.dismiss()
+                accountDialog.dismiss()
                 context?.let { HabiticaBaseApplication.logout(it) }
                 activity?.finish()
             }) { throwable ->
@@ -450,17 +435,13 @@ class AccountPreferenceFragment :
     }
 
     private fun showAccountResetConfirmation() {
-        val context = context ?: return
+        val habiticaAccountDialog = context?.let { HabiticaAccountDialog(it, "reset_account", this) }
+        habiticaAccountDialog?.show(parentFragmentManager, "account")
 
-        val dialog = HabiticaAlertDialog(context)
-        dialog.setTitle(R.string.reset_account)
-        dialog.setMessage(R.string.reset_account_description)
-        dialog.addButton(R.string.reset_account_confirmation, true, true) { _, _ ->
-            resetAccount()
+        if (habiticaAccountDialog != null) {
+            accountDialog = habiticaAccountDialog
         }
-        dialog.addCancelButton()
-        dialog.setAdditionalContentSidePadding(12.dpToPx(context))
-        dialog.show()
+
     }
 
     private fun showConfirmUsernameDialog() {
@@ -479,7 +460,10 @@ class AccountPreferenceFragment :
     private fun resetAccount() {
         val dialog = HabiticaProgressDialog.show(context, R.string.resetting_account)
         compositeSubscription.add(
-            userRepository.resetAccount().subscribe({ dialog?.dismiss() }) { throwable ->
+            userRepository.resetAccount().subscribe({
+                dialog?.dismiss()
+                accountDialog.dismiss()
+            }) { throwable ->
                 dialog?.dismiss()
                 RxErrorHandler.reportError(throwable)
             }
@@ -497,4 +481,13 @@ class AccountPreferenceFragment :
 
     override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
     }
+
+    override fun resetConfirmedClicked() {
+        resetAccount()
+    }
+
+    override fun deletionConfirmClicked(confirmationString: String) {
+        deleteAccount(confirmationString)
+    }
+
 }
---
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/fragments/preferences/HabiticaAccountDialog.kt
@@ -0,0 +1,137 @@
+package com.habitrpg.android.habitica.ui.fragments.preferences
+
+import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.habitrpg.android.habitica.R
+
+
+class HabiticaAccountDialog(private var thisContext: Context, private val accountAction: String, val accountUpdateConfirmed: AccountUpdateConfirmed) : DialogFragment() {
+
+    private lateinit var mainView: View
+    private var backBtn: ImageButton? = null
+    private var title: TextView? = null
+    private var warningDescription: TextView? = null
+    private var confirmationTextInputLayout: TextInputLayout? = null
+    private var confirmationText: TextInputEditText? = null
+    private var confirmationAction: TextView? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        mainView = inflater.inflate(R.layout.dialog_habitica_account, container, false)
+        return mainView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+
+        when (accountAction) {
+            "reset_account" -> setResetAccountViews()
+            "delete_account" -> setDeleteAccountViews()
+        }
+
+    }
+
+    private fun setResetAccountViews() {
+        title?.setText(R.string.reset_account_title)
+        warningDescription?.setText(R.string.reset_account_description)
+        confirmationTextInputLayout?.setHint(R.string.confirm_reset)
+        confirmationAction?.setText(R.string.reset_account)
+
+        confirmationText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.gray_10))
+                confirmationAction?.alpha = .4f
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (confirmationText?.text.toString() == context?.getString(R.string.reset_caps)) {
+                    confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.red_100))
+                    confirmationAction?.alpha = 1.0f
+                } else {
+                    confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.gray_10))
+                    confirmationAction?.alpha = .4f
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        confirmationAction?.setOnClickListener {
+            if (confirmationText?.text.toString() == context?.getString(R.string.reset_caps)) {
+                accountUpdateConfirmed.resetConfirmedClicked()
+            }
+        }
+    }
+
+    private fun setDeleteAccountViews() {
+        title?.setText(R.string.are_you_sure_you_want_to_delete)
+        warningDescription?.setText(R.string.delete_account_description)
+        confirmationTextInputLayout?.setHint(R.string.password)
+        confirmationAction?.setText(R.string.delete_account)
+
+        confirmationText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.gray_10))
+                confirmationAction?.alpha = .4f
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (confirmationText?.length() != null) {
+                    if (confirmationText?.length()!! > 7) {
+                        confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.red_100))
+                        confirmationAction?.alpha = 1.0f
+                    } else {
+                        confirmationAction?.setTextColor(ContextCompat.getColor(thisContext, R.color.gray_10))
+                        confirmationAction?.alpha = .4f
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        confirmationAction?.setOnClickListener {
+            if (confirmationText?.length() != null) {
+                if (confirmationText?.length()!! > 7) {
+                    accountUpdateConfirmed.deletionConfirmClicked(confirmationText?.text.toString())
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+        backBtn = mainView.findViewById(R.id.back_imagebutton)
+        backBtn?.setOnClickListener { this.dismiss() }
+        title = mainView.findViewById(R.id.title_textview)
+        warningDescription = mainView.findViewById(R.id.warning_description_textview)
+        confirmationTextInputLayout = mainView.findViewById(R.id.confirmation_text_input_layout)
+        confirmationText = mainView.findViewById(R.id.confirmation_input_edittext)
+        confirmationAction = mainView.findViewById(R.id.confirm_action_textview)
+    }
+
+    override fun getTheme(): Int {
+        return R.style.HabiticaAccountDialogTheme
+    }
+
+
+    interface AccountUpdateConfirmed {
+        //API currently does not take password to reset account
+        fun resetConfirmedClicked()
+        fun deletionConfirmClicked(confirmationString: String)
+    }
+
+
+}
+
