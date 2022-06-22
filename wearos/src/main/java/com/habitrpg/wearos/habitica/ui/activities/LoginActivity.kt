1a829f58a063c0556da8a4b85ba418f9310e7915
Phillip Thelen <phillip@habitica.com>
2022-06-22 10:33:51 +0200

    rework login screen

--- wearos/src/main/java/com/habitrpg/wearos/habitica/ui/activities/LoginActivity.kt
+++ wearos/src/main/java/com/habitrpg/wearos/habitica/ui/activities/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import com.habitrpg.android.habitica.R
 import com.habitrpg.android.habitica.databinding.ActivityLoginBinding
 import com.habitrpg.wearos.habitica.ui.viewmodels.LoginViewModel
@@ -14,7 +15,51 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity: BaseActivity<ActivityLoginBinding, LoginViewModel>() {
+    enum class State {
+        INITIAL,
+        OTHER,
+        INPUT
+    }
     override val viewModel: LoginViewModel by viewModels()
+    var currentState: State = State.INITIAL
+    set(value) {
+        field = value
+        when(value) {
+            State.INITIAL -> {
+                binding.descriptionView.isVisible = true
+                binding.signInOnPhoneButton.isVisible = true
+                binding.otherButton.isVisible = true
+                binding.googleLoginButton.isVisible = false
+                binding.registerButton.isVisible = false
+                binding.usernamePasswordButton.isVisible = false
+                binding.usernameEditText.isVisible = false
+                binding.passwordEditText.isVisible = false
+                binding.loginButton.isVisible = false
+            }
+            State.OTHER -> {
+                binding.descriptionView.isVisible = false
+                binding.signInOnPhoneButton.isVisible = false
+                binding.otherButton.isVisible = false
+                binding.googleLoginButton.isVisible = true
+                binding.registerButton.isVisible = true
+                binding.usernamePasswordButton.isVisible = true
+                binding.usernameEditText.isVisible = false
+                binding.passwordEditText.isVisible = false
+                binding.loginButton.isVisible = false
+            }
+            State.INPUT -> {
+                binding.descriptionView.isVisible = false
+                binding.signInOnPhoneButton.isVisible = false
+                binding.otherButton.isVisible = false
+                binding.googleLoginButton.isVisible = false
+                binding.registerButton.isVisible = false
+                binding.usernamePasswordButton.isVisible = false
+                binding.usernameEditText.isVisible = true
+                binding.passwordEditText.isVisible = true
+                binding.loginButton.isVisible = true
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -24,9 +69,15 @@ class LoginActivity: BaseActivity<ActivityLoginBinding, LoginViewModel>() {
             startMainActivity()
         }
 
+        binding.signInOnPhoneButton.setOnClickListener {  }
+        binding.otherButton.setOnClickListener { currentState = State.OTHER }
+        binding.usernamePasswordButton.setOnClickListener { currentState = State.INPUT }
+
         binding.loginButton.setOnClickListener { loginLocal() }
         binding.googleLoginButton.setOnClickListener { loginGoogle() }
         binding.registerButton.setOnClickListener { openRegisterOnPhone() }
+
+        currentState = State.INITIAL
     }
 
     private fun openRegisterOnPhone() {
