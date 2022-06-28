d364c3b22d9b9165416861debd5b2c23a8f2d5a5
Hafiz <hafizbhamidi@gmail.com>
2022-06-28 08:31:30 -0400

    Set header text to white if settings

--- wearos/src/main/java/com/habitrpg/wearos/habitica/ui/viewHolders/HeaderViewHolder.kt
+++ wearos/src/main/java/com/habitrpg/wearos/habitica/ui/viewHolders/HeaderViewHolder.kt
@@ -1,12 +1,17 @@
 package com.habitrpg.wearos.habitica.ui.viewHolders
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.habitrpg.android.habitica.R
 import com.habitrpg.android.habitica.databinding.RowHeaderBinding
 
 class HeaderViewHolder(itemView: View): BindableViewHolder<String>(itemView) {
     private val binding = RowHeaderBinding.bind(itemView)
 
     override fun bind(data: String) {
+        if (data == itemView.context.resources.getString(R.string.settings)) {
+            binding.textView.setTextColor(ContextCompat.getColor(itemView.context, R.color.white))
+        }
         binding.textView.text = data
     }
 }
