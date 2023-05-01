c0a45a678f82b86d096673ebf918aa8a69089575
Hafiz <hafizbhamidi@gmail.com>
2023-05-01 00:38:00 -0400

    Re-add message that show's when you don't have all the items / Check armoire count

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/inventory/ShopRecyclerAdapter.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/inventory/ShopRecyclerAdapter.kt
@@ -288,9 +288,15 @@ class ShopRecyclerAdapter : androidx.recyclerview.widget.RecyclerView.Adapter<Vi
 
         fun bind(className: String, armoireCount: Int) {
             binding.titleView.text = itemView.context.getString(R.string.shop_armoire_title, className)
-            binding.descriptionView.text = itemView.context.getString(R.string.shop_armoire_description)
-            binding.iconView.visibility = View.GONE
-            binding.footerLayout.visibility = View.INVISIBLE
+            if (armoireCount > 0) {
+                binding.descriptionView.text = itemView.context.getString(R.string.shop_armoire_description, armoireCount)
+                binding.footerLayout.visibility = View.VISIBLE
+                binding.iconView.visibility = View.VISIBLE
+            } else {
+                binding.descriptionView.text = itemView.context.getString(R.string.shop_armoire_empty_description)
+                binding.footerLayout.visibility = View.INVISIBLE
+                binding.iconView.visibility = View.GONE
+            }
         }
     }
 }
