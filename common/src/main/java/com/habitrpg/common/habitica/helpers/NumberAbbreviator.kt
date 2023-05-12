51a369b88fff6d829638d0de4ed00a8f1013af0f
Hafiz <hafizbhamidi@gmail.com>
2023-05-12 08:52:23 -0400

    Update number abbreviator

--- common/src/main/java/com/habitrpg/common/habitica/helpers/NumberAbbreviator.kt
+++ common/src/main/java/com/habitrpg/common/habitica/helpers/NumberAbbreviator.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.habitrpg.common.habitica.R
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 import kotlin.math.abs
 
 object NumberAbbreviator {
@@ -28,14 +30,26 @@ object NumberAbbreviator {
         if (number < 0) {
             pattern = "-$pattern"
         }
+
+        // Initially use US symbols for formatting
+        val symbols = DecimalFormatSymbols.getInstance(Locale.US)
         val formatter = DecimalFormat(
             pattern + abbreviationForCounter(context, counter)
                 .replace(".", "")
-                .replace(",", "")
+                .replace(",", ""),
+            symbols
         )
         formatter.roundingMode = RoundingMode.FLOOR
-        return formatter.format(usedNumber)
+        var formattedString = formatter.format(usedNumber)
+
+        // Get the decimal separator of the current locale and replace the decimal separator with the current locale.
+        val currentLocaleSymbols = DecimalFormatSymbols.getInstance()
+        val currentLocaleDecimalSeparator = currentLocaleSymbols.decimalSeparator
+        formattedString = formattedString.replace('.', currentLocaleDecimalSeparator)
+
+        return formattedString
     }
+
 
     private fun abbreviationForCounter(context: Context?, counter: Int): String = when (counter) {
         1 -> context?.getString(R.string.thousand_abbrev) ?: "k"
