a591e74311e00360e92275610248682f7be67a6e
Hafiz <hafizbhamidi@gmail.com>
2023-05-28 19:43:31 -0400

    Refresh views without "flash"

--- common/src/main/java/com/habitrpg/common/habitica/extensions/ViewExt.kt
+++ common/src/main/java/com/habitrpg/common/habitica/extensions/ViewExt.kt
@@ -1,5 +1,6 @@
 package com.habitrpg.android.habitica.extensions
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.view.View
 import android.view.ViewTreeObserver
@@ -28,3 +29,13 @@ inline fun View.afterMeasured(crossinline f: View.() -> Unit) {
         }
     })
 }
+
+fun View.fadeInAnimation(duration: Long = 500) {
+    this.alpha = 0f
+    this.visibility = View.VISIBLE
+
+    val fadeInAnimation = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+    fadeInAnimation.duration = duration
+    fadeInAnimation.start()
+}
+
