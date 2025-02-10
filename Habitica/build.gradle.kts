a508596fbf3c62a512fe53250059ed2ae1b30cc3
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-02-10 01:02:12 +0000

    Update build.gradle.kts

--- Habitica/build.gradle.kts
+++ Habitica/build.gradle.kts
@@ -50,10 +50,10 @@ android {
 
     signingConfigs {
         create("releaseSigning") {
-            keyAlias = 'habitica'         // Alias of the key in the keystore
-            keyPassword = '12345678'   // Password for the key
+            keyAlias = "habitica"         // Alias of the key in the keystore
+            keyPassword = "12345678"   // Password for the key
             storeFile = file("./signature/habitica.keystore")  // Keystore file path
-            storePassword = '12345678'  // Keystore password
+            storePassword = "12345678"  // Keystore password
         }
     } 
 
