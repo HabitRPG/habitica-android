0a0e50543a18ae08d0f879a61808db8ed0e69c98
Phillip Thelen <phillip@habitica.com>
2025-02-19 17:09:42 +0100

    gem fixes

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/BaseActivity.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/activities/BaseActivity.kt
@@ -99,7 +99,7 @@ abstract class BaseActivity : AppCompatActivity() {
     internal var navigationBarStyle: SystemBarStyle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(navigationBarStyle = navigationBarStyle ?: defaultNavigationBarStyle)
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         val languageHelper = LanguageHelper(sharedPreferences.getString("language", "en"))
         resources.forceLocale(this, languageHelper.locale)
