dff1b1fce6350c05719be2a8422797d819cdcde5
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-02-20 21:50:06 +0000

    Update Frequency.kt
    pt_PT

--- shared/src/commonMain/kotlin/com/habitrpg/shared/habitica/models/tasks/Frequency.kt
+++ shared/src/commonMain/kotlin/com/habitrpg/shared/habitica/models/tasks/Frequency.kt
@@ -1,10 +1,10 @@
 package com.habitrpg.shared.habitica.models.tasks
 
 enum class Frequency(val value: String) {
-    WEEKLY("weekly"),
-    DAILY("daily"),
-    MONTHLY("monthly"),
-    YEARLY("yearly")
+    WEEKLY("semanal"),
+    DAILY("diario"),
+    MONTHLY("mensal"),
+    YEARLY("anual")
     ;
 
     companion object {
