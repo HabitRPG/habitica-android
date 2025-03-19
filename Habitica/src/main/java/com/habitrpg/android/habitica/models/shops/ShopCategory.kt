1256c8362f9f6701cbb9f29737e1f7c9dc95dc91
Phillip Thelen <phillip@habitica.com>
2025-03-19 12:29:50 +0100

    Improve shop countdown display

--- Habitica/src/main/java/com/habitrpg/android/habitica/models/shops/ShopCategory.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/models/shops/ShopCategory.kt
@@ -1,6 +1,7 @@
 package com.habitrpg.android.habitica.models.shops
 
 import com.google.gson.annotations.SerializedName
+import io.realm.RealmList
 import java.util.Date
 
 class ShopCategory {
@@ -14,5 +15,17 @@ class ShopCategory {
     @SerializedName("end")
     var endDate: Date? = null
 
+    var itemEndDates: MutableList<Date> = ArrayList()
+
     var items: MutableList<ShopItem> = ArrayList()
+
+    val endDates: Set<Date>
+        get() {
+            val dates = itemEndDates.distinct().toMutableSet()
+            if (items.isNotEmpty()) {
+                dates.addAll(items.mapNotNull { it.endDate })
+            }
+            endDate?.let { dates.add(it) }
+            return dates
+        }
 }
