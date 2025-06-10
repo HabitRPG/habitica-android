34ff1894360d99b160b9ca3111c67b1e700863df
Hafiz <hafizbhamidi@gmail.com>
2025-06-09 19:17:37 -0500

    Fix GIF animation by checking Drawable
    In onSuccess, use if (drawable is Animatable) to start GIFs instead of checking the Coil Image result.

--- common/src/main/java/com/habitrpg/common/habitica/views/AvatarView.kt
+++ common/src/main/java/com/habitrpg/common/habitica/views/AvatarView.kt
@@ -205,8 +205,8 @@ class AvatarView : FrameLayout {
                             drawable.isFilterBitmap = false
                             super.onSuccess(result)
                             imageView.setImageDrawable(drawable)
-                            if (result is Animatable) {
-                                result.start()
+                            if (drawable is Animatable) {
+                                drawable.start()
                             }
                             val bounds = getLayerBounds(layerKey, layerName, drawable)
                             imageView.imageMatrix = avatarMatrix
