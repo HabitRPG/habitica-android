f7f8bb841cc520d756981eb1aa3f9411d41f817c
Hafiz <hafizbhamidi@gmail.com>
2025-07-03 15:12:06 -0500

    Removes redundant authentication check
    Removes a redundant check for "There is no account that uses
    those credentials" as it's already covered by other checks,
    simplifying the logic for handling invalid credentials and
    preventing unnecessary code execution.

--- Habitica/src/main/java/com/habitrpg/android/habitica/data/implementation/ApiClientImpl.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/data/implementation/ApiClientImpl.kt
@@ -178,8 +178,7 @@ class ApiClientImpl(
                                                 || errField.equals("invalidCredentials", ignoreCase = true)
                                                 || msgField.contains("invalidCredentials", ignoreCase = true)
                                                 || msgField.contains("Missing authentication headers", ignoreCase = true)
-                                                || msgField.contains("There is no account that uses those credentials", ignoreCase = true)
-
+                                        
                                         if (shouldLogout) {
                                             HabiticaBaseApplication.logout(context)
                                         }
