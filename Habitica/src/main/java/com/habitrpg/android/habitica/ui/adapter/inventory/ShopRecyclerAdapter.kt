cd3a7affdd74087b60e5f49e4d116c5c2117645f
diyoyo <6624639+diyoyo@users.noreply.github.com>
2025-09-25 18:14:06 +0200

    Adding plurals to handle shop_armoire quote problems
    - Converted string "shop_armoire_description" into a plurals where the 0 item is the actual "empty" description, and the two other items are written in proper English.
    - Update ShopRecyclerAdapter.kt accordingly

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/inventory/ShopRecyclerAdapter.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/adapter/inventory/ShopRecyclerAdapter.kt
@@ -392,10 +392,11 @@ class ShopRecyclerAdapter : androidx.recyclerview.widget.RecyclerView.Adapter<Vi
                 itemView.context.getString(R.string.shop_armoire_title, className)
             if (armoireCount > 0) {
                 binding.descriptionView.text =
-                    itemView.context.getString(R.string.shop_armoire_description, armoireCount)
+                    itemView.context.getQuantityString(R.plurals.shop_armoire_description, armoireCount, armoireCount)
                 binding.footerLayout.visibility = View.VISIBLE
                 binding.iconView.visibility = View.VISIBLE
             } else {
+                //the following won't be necessary with getQuantityString
                 binding.descriptionView.text =
                     itemView.context.getString(R.string.shop_armoire_empty_description)
                 binding.footerLayout.visibility = View.INVISIBLE
