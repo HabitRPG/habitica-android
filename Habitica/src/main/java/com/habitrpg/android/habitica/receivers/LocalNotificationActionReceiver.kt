bd79dd47c6e622c75616769dfccb4442ffda304e
Hafiz <hafizbhamidi@gmail.com>
2026-03-04 13:26:22 -0600

    Notification handling improvements
    also fixes accepting quest invite from push notifs

--- Habitica/src/main/java/com/habitrpg/android/habitica/receivers/LocalNotificationActionReceiver.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/receivers/LocalNotificationActionReceiver.kt
@@ -14,12 +14,10 @@ import com.habitrpg.android.habitica.data.SocialRepository
 import com.habitrpg.android.habitica.data.TaskRepository
 import com.habitrpg.android.habitica.data.UserRepository
 import com.habitrpg.android.habitica.interactors.NotifyUserUseCase
-import com.habitrpg.android.habitica.models.user.User
-import com.habitrpg.common.habitica.helpers.ExceptionHandler
 import com.habitrpg.common.habitica.helpers.launchCatching
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -36,7 +34,6 @@ class LocalNotificationActionReceiver : BroadcastReceiver() {
     @Inject
     lateinit var apiClient: ApiClient
 
-    private var user: User? = null
     private val groupID: String?
         get() = intent?.extras?.getString("groupID")
     private val senderID: String?
@@ -52,66 +49,61 @@ class LocalNotificationActionReceiver : BroadcastReceiver() {
     ) {
         this.intent = intent
         this.context = context
-        handleLocalNotificationAction(intent.action)
+        val pendingResult = goAsync()
+        MainScope().launchCatching {
+            try {
+                handleLocalNotificationAction(intent.action)
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 
-    private fun handleLocalNotificationAction(action: String?) {
+    private suspend fun handleLocalNotificationAction(action: String?) {
         val notificationManager =
             this.context?.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
         notificationManager?.cancel(intent?.extras?.getInt("NOTIFICATION_ID") ?: -1)
         when (action) {
             context?.getString(R.string.accept_party_invite) -> {
                 groupID?.let {
-                    MainScope().launch(ExceptionHandler.coroutine()) {
-                        socialRepository.joinGroup(it)
-                    }
+                    socialRepository.joinGroup(it)
                 }
             }
 
             context?.getString(R.string.reject_party_invite) -> {
                 groupID?.let {
-                    MainScope().launchCatching {
-                        socialRepository.rejectGroupInvite(it)
-                    }
+                    socialRepository.rejectGroupInvite(it)
                 }
             }
 
             context?.getString(R.string.accept_quest_invite) -> {
-                MainScope().launchCatching {
-                    socialRepository.acceptQuest(user)
-                }
+                val user = userRepository.getUser().firstOrNull()
+                socialRepository.acceptQuest(user)
             }
 
             context?.getString(R.string.reject_quest_invite) -> {
-                MainScope().launchCatching {
-                    socialRepository.rejectQuest(user)
-                }
+                val user = userRepository.getUser().firstOrNull()
+                socialRepository.rejectQuest(user)
             }
 
             context?.getString(R.string.accept_guild_invite) -> {
                 groupID?.let {
-                    MainScope().launch(ExceptionHandler.coroutine()) {
-                        socialRepository.joinGroup(it)
-                    }
+                    socialRepository.joinGroup(it)
                 }
             }
 
             context?.getString(R.string.reject_guild_invite) -> {
                 groupID?.let {
-                    MainScope().launchCatching {
-                        socialRepository.rejectGroupInvite(it)
-                    }
+                    socialRepository.rejectGroupInvite(it)
                 }
             }
 
             context?.getString(R.string.group_message_reply) -> {
                 groupID?.let {
                     getMessageText(context?.getString(R.string.group_message_reply))?.let { message ->
-                        MainScope().launchCatching {
-                            socialRepository.postGroupChat(it, message)
-                            context?.let { c ->
-                                NotificationManagerCompat.from(c).cancel(it.hashCode())
-                            }
+                        socialRepository.postGroupChat(it, message)
+                        context?.let { c ->
+                            NotificationManagerCompat.from(c).cancel(it.hashCode())
                         }
                     }
                 }
@@ -120,27 +112,23 @@ class LocalNotificationActionReceiver : BroadcastReceiver() {
             context?.getString(R.string.inbox_message_reply) -> {
                 senderID?.let {
                     getMessageText(context?.getString(R.string.inbox_message_reply))?.let { message ->
-                        MainScope().launch(ExceptionHandler.coroutine()) {
-                            socialRepository.postPrivateMessage(it, message)
-                        }
+                        socialRepository.postPrivateMessage(it, message)
                     }
                 }
             }
 
             context?.getString(R.string.complete_task_action) -> {
                 taskID?.let {
-                    MainScope().launch(ExceptionHandler.coroutine()) {
-                        taskRepository.taskChecked(null, it, up = true, force = false) {
-                            val pair =
-                                NotifyUserUseCase.getNotificationAndAddStatsToUserAsText(
-                                    it.experienceDelta,
-                                    it.healthDelta,
-                                    it.goldDelta,
-                                    it.manaDelta,
-                                    it.questDamage
-                                )
-                            showToast(pair.first)
-                        }
+                    taskRepository.taskChecked(null, it, up = true, force = false) {
+                        val pair =
+                            NotifyUserUseCase.getNotificationAndAddStatsToUserAsText(
+                                it.experienceDelta,
+                                it.healthDelta,
+                                it.goldDelta,
+                                it.manaDelta,
+                                it.questDamage
+                            )
+                        showToast(pair.first)
                     }
                 }
             }
