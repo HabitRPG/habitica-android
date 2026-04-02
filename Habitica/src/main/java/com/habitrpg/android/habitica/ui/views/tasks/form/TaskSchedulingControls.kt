7309d2ce70f5a8e056ff5a913bba11efcc11d42a
Hafiz <hafizbhamidi@gmail.com>
2026-04-02 17:28:06 -0500

    update monthly weeksOfMonth summary wording and add 5th week warning

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/tasks/form/TaskSchedulingControls.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/tasks/form/TaskSchedulingControls.kt
@@ -405,7 +405,7 @@ constructor(
             when (frequency) {
                 Frequency.DAILY -> if (everyX == 1) "day" else "days"
                 Frequency.WEEKLY -> if (everyX == 1) "week" else "weeks"
-                Frequency.MONTHLY -> if (everyX == 1) "month" else "months"
+                Frequency.MONTHLY -> context.getString(if (everyX == 1) R.string.month else R.string.months)
                 Frequency.YEARLY -> if (everyX == 1) "year" else "years"
             }
 
@@ -455,20 +455,28 @@ constructor(
                             Calendar.LONG,
                             LanguageHelper.systemLocale
                         )
-                    " on the $formattedWeek week on $dayLongName"
+                    " on the $formattedWeek $dayLongName of the month"
                 }
         }
 
         val everyXString = if (everyX == 1) "" else "$everyX "
 
-        val summary =
+        var summary =
             resources.getString(
                 R.string.repeat_summary,
-                frequency,
                 everyXString,
                 frequencyQualifier,
                 weekdays
             )
+        if (frequency == Frequency.MONTHLY && weeksOfMonth?.contains(4) == true) {
+            val dayName =
+                startDateCalendar.getDisplayName(
+                    Calendar.DAY_OF_WEEK,
+                    Calendar.LONG,
+                    LanguageHelper.systemLocale
+                )
+            summary += " " + context.getString(R.string.fifth_week_warning, dayName)
+        }
         binding.summaryTextview.text = summary
     }
 }
