678af3e613a9050f86d12f3002a7d246a3acd45e
Phillip Thelen <phillip@thelen.space>
2026-06-03 13:02:49 +0200

    refactor realm

--- Habitica/src/main/java/com/habitrpg/android/habitica/models/inventory/QuestColors.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/models/inventory/QuestColors.kt
@@ -1,13 +1,8 @@
 package com.habitrpg.android.habitica.models.inventory
 
-import androidx.core.graphics.toColorInt
 import com.habitrpg.android.habitica.models.BaseObject
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-
-/**
- * Created by phillip on 31.01.18.
- */
 
 open class QuestColors : RealmObject(), BaseObject {
     @PrimaryKey
@@ -16,21 +11,4 @@ open class QuestColors : RealmObject(), BaseObject {
     var medium: String? = null
     var light: String? = null
     var extralight: String? = null
-
-    val darkColor: Int
-        get() {
-            return dark.toColorInt()
-        }
-    val mediumColor: Int
-        get() {
-            return medium.toColorInt()
-        }
-    val lightColor: Int
-        get() {
-            return light.toColorInt()
-        }
-    val extraLightColor: Int
-        get() {
-            return extralight.toColorInt()
-        }
 }
