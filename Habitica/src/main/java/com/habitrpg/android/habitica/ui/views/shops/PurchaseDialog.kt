93bfb6726fda658f24dd38805075efcb979dd37b
Phillip Thelen <phillip@thelen.space>
2026-08-03 17:12:28 +0200

    handle error in purchase dialog

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/shops/PurchaseDialog.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/shops/PurchaseDialog.kt
@@ -399,7 +399,9 @@ class PurchaseDialog(
             } else {
                 limitedTextView.text = context.getString(R.string.gems_left_nomax, gemsLeft)
             }
-            item.limitedNumberLeft = gemsLeft
+            if (!item.isManaged) {
+                item.limitedNumberLeft = gemsLeft
+            }
             limitedTextView.visibility = View.VISIBLE
             if (gemsLeft == 0) {
                 limitedTextView.setBackgroundColor(ContextCompat.getColor(context, R.color.orange_10))
