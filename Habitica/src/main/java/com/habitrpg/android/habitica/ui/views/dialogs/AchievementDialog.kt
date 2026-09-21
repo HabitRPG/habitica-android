967827f88ded008c33d74dfcf908d4c061b6ea85
Phillip Thelen <phillip@thelen.space>
2026-09-21 13:25:24 +0200

    fix filename

--- Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/dialogs/AchievementDialog.kt
+++ Habitica/src/main/java/com/habitrpg/android/habitica/ui/views/dialogs/AchievementDialog.kt
@@ -166,7 +166,7 @@ class AchievementDialog(
     ) {
         binding.titleView.text = title
         binding.descriptionView.setText(description.fromHtml(), TextView.BufferType.SPANNABLE)
-        binding.iconView.loadImage("achievement-${iconName}2x")
+        binding.iconView.loadImage(if (iconName.startsWith("achievement-")) "${iconName}2x" else "achievement-${iconName}2x")
         if (iconName == "onboardingComplete") {
             setTitle(R.string.onboardingComplete_achievement_title)
             binding.titleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
